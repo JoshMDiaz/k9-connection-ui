881cdbd65dd24d983e249c29874246d631834978
Josh Diaz <josh@iserveproducts.com>
2020-05-16 22:14:58 -0600

    Pagination

--- src/components/Pages/Dogs.js
+++ src/components/Pages/Dogs.js
@@ -3,41 +3,64 @@ import List from '../Dogs/List'
 import DogService from '../../services/DogService'
 import LoadingCard from '../common/LoadingCard/LoadingCard'
 import FilterHelperService from '../../services/FilterHelperService'
+import { Pagination } from '@material-ui/lab'
 
 const Dogs = ({ filter, callout }) => {
   const [state, setState] = useState({
     dogs: [],
     loading: true,
+    page: 1,
+    total: null,
   })
-  const { dogs, loading } = state
+  const { dogs, loading, page, total } = state
 
-  const getDogs = useCallback(() => {
-    setState((prevState) => ({
-      ...prevState,
-      loading: true,
-    }))
-    let params = FilterHelperService.generateParams(filter)
-    DogService.getAll(params).then((response) => {
-      if (response) {
-        let dogsArr = [...response.data]
-        callout(dogsArr.length)
-        setState((prevState) => ({
-          ...prevState,
-          dogs: dogsArr,
-          loading: false,
-        }))
-      }
-    })
-  }, [callout, filter])
+  const getDogs = useCallback(
+    (pageNumber) => {
+      setState((prevState) => ({
+        ...prevState,
+        loading: true,
+      }))
+      localStorage.setItem('dogPageNumber', pageNumber)
+      let params = FilterHelperService.generateParams(filter)
+      params.page = pageNumber
+      DogService.getAll(params).then((response) => {
+        if (response) {
+          let dogsArr = [...response.data]
+          callout(dogsArr[0].count)
+          setState((prevState) => ({
+            ...prevState,
+            dogs: dogsArr,
+            loading: false,
+            page: Number(pageNumber),
+            total: dogsArr[0].count,
+          }))
+        }
+      })
+    },
+    [callout, filter]
+  )
 
   useEffect(() => {
-    getDogs()
-  }, [getDogs, filter])
+    getDogs(localStorage.getItem('dogPageNumber') || 1)
+  }, [getDogs])
+
+  let totalCount = Math.round(total / 20)
 
   return (
     <div className='page-padding'>
       {!loading ? (
-        <List dogs={dogs} />
+        <>
+          <List dogs={dogs} />
+          <Pagination
+            count={totalCount}
+            page={page}
+            showFirstButton={page > 1 && totalCount > 20}
+            showLastButton={page !== totalCount && totalCount > 20}
+            hidePrevButton={page === 1}
+            hideNextButton={page === totalCount}
+            onChange={(_, pageNum) => getDogs(pageNum)}
+          />
+        </>
       ) : (
         <div className='card-list'>
           {[...Array(12).keys()].map((row, i) => {
